e614647a68c6e85b11c7f3a507735e5bfc1bcda5
Johan Sageryd <j@1616.se>
2018-12-03 22:27:00 +0100

    Correct test output

--- 2017/day-05/main_test.go
+++ 2017/day-05/main_test.go
@@ -10,7 +10,7 @@ func TestMazeSteps(t *testing.T) {
 		{[]int{0, 3, 0, 1, -3}, 5},
 	} {
 		if got, want := mazeSteps(tc.in), tc.out; got != want {
-			t.Errorf("[%d] mazeSteps(%v) = %d, want %d", n, tc.in, got, want)
+			t.Errorf("[%d] got %d, want %d", n, got, want)
 		}
 	}
 }
@@ -23,7 +23,7 @@ func TestMazeStepsAlternate(t *testing.T) {
 		{[]int{0, 3, 0, 1, -3}, 10},
 	} {
 		if got, want := mazeStepsAlternate(tc.in), tc.out; got != want {
-			t.Errorf("[%d] mazeStepsAlternate(%v) = %d, want %d", n, tc.in, got, want)
+			t.Errorf("[%d] got %d, want %d", n, got, want)
 		}
 	}
 }
