9fadfacfa9738f0a9bc81c0f6b4f424ce4303e75
Johan Sageryd <j@1616.se>
2018-12-11 15:42:24 +0100

    2018 Day 11: Chronal Charge

---
+++ 2018/day-11/main.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+func main() {
+	var input int
+	fmt.Scanf("%d", &input)
+
+	var x, y, side int
+
+	x, y, _ = find3x3Square(input)
+	fmt.Printf("Part 1: %d,%d\n", x, y)
+
+	x, y, side, _ = findNxNSquare(input)
+	fmt.Printf("Part 2: %d,%d,%d\n", x, y, side)
+}
+
+func find3x3Square(gridSerialNumber int) (x, y, totalPower int) {
+	for yy := 1; yy <= 300-2; yy++ {
+		for xx := 1; xx <= 300-2; xx++ {
+			sum := 0
+			for oy := 0; oy < 3; oy++ {
+				for ox := 0; ox < 3; ox++ {
+					sum += powerLevel(gridSerialNumber, xx+ox, yy+oy)
+				}
+			}
+			if sum > totalPower {
+				x, y, totalPower = xx, yy, sum
+			}
+		}
+	}
+	return x, y, totalPower
+}
+
+func findNxNSquare(gridSerialNumber int) (x, y, side, totalPower int) {
+	for squareSide := 1; squareSide <= 300; squareSide++ {
+		for yy := 1; yy <= 300-squareSide+1; yy++ {
+			for xx := 1; xx <= 300-squareSide+1; xx++ {
+				sum := 0
+				for oy := 0; oy < squareSide; oy++ {
+					for ox := 0; ox < squareSide; ox++ {
+						sum += powerLevel(gridSerialNumber, xx+ox, yy+oy)
+					}
+				}
+				if sum > totalPower {
+					x, y, side, totalPower = xx, yy, squareSide, sum
+				}
+			}
+		}
+	}
+	return x, y, side, totalPower
+}
+
+func powerLevel(gridSerialNumber, x, y int) int {
+	rackID := x + 10
+	powerLevel := rackID * y
+	powerLevel += gridSerialNumber
+	powerLevel *= rackID
+	powerLevel = powerLevel/100 - powerLevel/1000*10
+	powerLevel -= 5
+	return powerLevel
+}
---
+++ 2018/day-11/main_test.go
@@ -0,0 +1,59 @@
+package main
+
+import "testing"
+
+func TestFind3x3Square(t *testing.T) {
+	for n, tc := range []struct {
+		gridSerialNumber, x, y, totalPower int
+	}{
+		{18, 33, 45, 29},
+		{42, 21, 61, 30},
+	} {
+		x, y, totalPower := find3x3Square(tc.gridSerialNumber)
+
+		if x != tc.x || y != tc.y || totalPower != tc.totalPower {
+			t.Errorf(
+				"[%d] find3x3Square(%d) = %d, %d, %d; want %d, %d, %d",
+				n, tc.gridSerialNumber, x, y, totalPower, tc.x, tc.y, tc.totalPower,
+			)
+		}
+	}
+}
+
+func TestFindNxNSquare(t *testing.T) {
+	for n, tc := range []struct {
+		gridSerialNumber, x, y, side, totalPower int
+	}{
+		{18, 90, 269, 16, 113},
+		{42, 232, 251, 12, 119},
+	} {
+		x, y, side, totalPower := findNxNSquare(tc.gridSerialNumber)
+
+		if x != tc.x || y != tc.y || side != tc.side || totalPower != tc.totalPower {
+			t.Errorf(
+				"[%d] findNxNSquare(%d) = %d, %d, %d, %d; want %d, %d, %d, %d",
+				n, tc.gridSerialNumber, x, y, side, totalPower, tc.x, tc.y, tc.side, tc.totalPower,
+			)
+		}
+	}
+}
+
+func TestPowerLevel(t *testing.T) {
+	for n, tc := range []struct {
+		gridSerialNumber, x, y, powerLevel int
+	}{
+		{8, 3, 5, 4},
+		{57, 122, 79, -5},
+		{39, 217, 196, 0},
+		{71, 101, 153, 4},
+	} {
+		powerLevel := powerLevel(tc.gridSerialNumber, tc.x, tc.y)
+
+		if powerLevel != tc.powerLevel {
+			t.Errorf(
+				"[%d] powerLevel(%d, %d, %d) = %d; want %d",
+				n, tc.gridSerialNumber, tc.x, tc.y, powerLevel, tc.powerLevel,
+			)
+		}
+	}
+}
