a2160c3a4d3cfbf5e1f05b5d9a280f11e83243fc
Johan Sageryd <j@1616.se>
2018-12-28 23:12:14 +0100

    2018 Day 19: Go With The Flow; part 1

---
+++ 2018/day-19/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var input []string
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+
+	var reg [6]int
+
+	run(&reg, input)
+
+	fmt.Printf("Part 1: %d\n", reg[0])
+}
+
+type Inst struct {
+	op      string
+	a, b, c int
+}
+
+func run(reg *[6]int, programStrs []string) {
+	var ipReg int
+	fmt.Sscanf(programStrs[0], "#ip %d", &ipReg)
+	programStrs = programStrs[1:]
+
+	program := make([]Inst, 0, len(programStrs))
+
+	for n := range programStrs {
+		var inst Inst
+		fmt.Sscanf(programStrs[n], "%s %d %d %d", &inst.op, &inst.a, &inst.b, &inst.c)
+		program = append(program, inst)
+	}
+
+	for ip := 0; ip >= 0 && ip < len(program); ip++ {
+		inst := program[ip]
+		reg[ipReg] = ip
+		ops[inst.op](reg, inst.a, inst.b, inst.c)
+		ip = reg[ipReg]
+	}
+}
+
+var ops = map[string]func(reg *[6]int, a, b, c int){
+	"addr": func(reg *[6]int, a, b, c int) { reg[c] = reg[a] + reg[b] },
+	"addi": func(reg *[6]int, a, b, c int) { reg[c] = reg[a] + b },
+	"mulr": func(reg *[6]int, a, b, c int) { reg[c] = reg[a] * reg[b] },
+	"muli": func(reg *[6]int, a, b, c int) { reg[c] = reg[a] * b },
+	"banr": func(reg *[6]int, a, b, c int) { reg[c] = reg[a] & reg[b] },
+	"bani": func(reg *[6]int, a, b, c int) { reg[c] = reg[a] & b },
+	"borr": func(reg *[6]int, a, b, c int) { reg[c] = reg[a] | reg[b] },
+	"bori": func(reg *[6]int, a, b, c int) { reg[c] = reg[a] | b },
+	"setr": func(reg *[6]int, a, b, c int) { reg[c] = reg[a] },
+	"seti": func(reg *[6]int, a, b, c int) { reg[c] = a },
+	"gtir": func(reg *[6]int, a, b, c int) {
+		if a > reg[b] {
+			reg[c] = 1
+		} else {
+			reg[c] = 0
+		}
+	},
+	"gtri": func(reg *[6]int, a, b, c int) {
+		if reg[a] > b {
+			reg[c] = 1
+		} else {
+			reg[c] = 0
+		}
+	},
+	"gtrr": func(reg *[6]int, a, b, c int) {
+		if reg[a] > reg[b] {
+			reg[c] = 1
+		} else {
+			reg[c] = 0
+		}
+	},
+	"eqir": func(reg *[6]int, a, b, c int) {
+		if a == reg[b] {
+			reg[c] = 1
+		} else {
+			reg[c] = 0
+		}
+	},
+	"eqri": func(reg *[6]int, a, b, c int) {
+		if reg[a] == b {
+			reg[c] = 1
+		} else {
+			reg[c] = 0
+		}
+	},
+	"eqrr": func(reg *[6]int, a, b, c int) {
+		if reg[a] == reg[b] {
+			reg[c] = 1
+		} else {
+			reg[c] = 0
+		}
+	},
+}
---
+++ 2018/day-19/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestRun(t *testing.T) {
+	program := []string{
+		"#ip 0",
+		"seti 5 0 1",
+		"seti 6 0 2",
+		"addi 0 1 0",
+		"addr 1 2 3",
+		"setr 1 0 0",
+		"seti 8 0 4",
+		"seti 9 0 5",
+	}
+
+	wantReg := [6]int{6, 5, 6, 0, 0, 9}
+
+	var reg [6]int
+
+	run(&reg, program)
+
+	if reg != wantReg {
+		t.Errorf("got %v, want %v", reg, wantReg)
+	}
+}
