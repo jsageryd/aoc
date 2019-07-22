00787427effde40d8cc1089995f9b61b25e71fa9
Johan Sageryd <j@1616.se>
2019-07-23 01:00:36 +0200

    2015 Day 3: Perfectly Spherical Houses in a Vacuum; part 2

--- 2015/day-03/main.go
+++ 2015/day-03/main.go
@@ -9,30 +9,33 @@ import (
 func main() {
 	input, _ := ioutil.ReadAll(os.Stdin)
 
-	fmt.Printf("Part 1: %d\n", len(findHouses(input)))
+	fmt.Printf("Part 1: %d\n", len(findHouses(input, 1)))
+	fmt.Printf("Part 2: %d\n", len(findHouses(input, 2)))
 }
 
 type coord struct {
 	x, y int
 }
 
-func findHouses(input []byte) map[coord]int {
-	var c coord
+func findHouses(input []byte, santas int) map[coord]int {
+	m := map[coord]int{coord{0, 0}: santas}
 
-	m := map[coord]int{c: 1}
+	var currentSanta int
+	cs := make([]coord, santas)
 
 	for _, b := range input {
+		currentSanta = (currentSanta + 1) % santas
 		switch b {
 		case '>':
-			c.x++
+			cs[currentSanta].x++
 		case '<':
-			c.x--
+			cs[currentSanta].x--
 		case '^':
-			c.y--
+			cs[currentSanta].y--
 		case 'v':
-			c.y++
+			cs[currentSanta].y++
 		}
-		m[c]++
+		m[cs[currentSanta]]++
 	}
 
 	return m
--- 2015/day-03/main_test.go
+++ 2015/day-03/main_test.go
@@ -5,17 +5,20 @@ import "testing"
 func TestFindHouses(t *testing.T) {
 	for n, tc := range []struct {
 		input  []byte
+		santas int
 		houses map[coord]int
 	}{
 		{
-			input: []byte(">"),
+			input:  []byte(">"),
+			santas: 1,
 			houses: map[coord]int{
 				coord{0, 0}: 1,
 				coord{1, 0}: 1,
 			},
 		},
 		{
-			input: []byte("^>v<"),
+			input:  []byte("^>v<"),
+			santas: 1,
 			houses: map[coord]int{
 				coord{0, 0}:  2,
 				coord{0, -1}: 1,
@@ -24,14 +27,50 @@ func TestFindHouses(t *testing.T) {
 			},
 		},
 		{
-			input: []byte("^v^v^v^v^v"),
+			input:  []byte("^v^v^v^v^v"),
+			santas: 1,
 			houses: map[coord]int{
 				coord{0, 0}:  6,
 				coord{0, -1}: 5,
 			},
 		},
+		{
+			input:  []byte("^v"),
+			santas: 2,
+			houses: map[coord]int{
+				coord{0, 0}:  2,
+				coord{0, -1}: 1,
+				coord{0, 1}:  1,
+			},
+		},
+		{
+			input:  []byte("^>v<"),
+			santas: 2,
+			houses: map[coord]int{
+				coord{0, 0}:  4,
+				coord{0, -1}: 1,
+				coord{1, 0}:  1,
+			},
+		},
+		{
+			input:  []byte("^v^v^v^v^v"),
+			santas: 2,
+			houses: map[coord]int{
+				coord{0, 0}:  2,
+				coord{0, -1}: 1,
+				coord{0, -2}: 1,
+				coord{0, -3}: 1,
+				coord{0, -4}: 1,
+				coord{0, -5}: 1,
+				coord{0, 1}:  1,
+				coord{0, 2}:  1,
+				coord{0, 3}:  1,
+				coord{0, 4}:  1,
+				coord{0, 5}:  1,
+			},
+		},
 	} {
-		gotHouses := findHouses(tc.input)
+		gotHouses := findHouses(tc.input, tc.santas)
 
 		if len(gotHouses) != len(tc.houses) {
 			t.Fatalf("[%d] got %v, want %v", n, gotHouses, tc.houses)
