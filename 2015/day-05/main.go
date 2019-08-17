d132587da019e729be6bbb719702ee58f61aaf60
Johan Sageryd <j@1616.se>
2019-08-17 11:49:48 +0200

    2015 Day 5: Doesn't He Have Intern-Elves For This?; part 2

--- 2015/day-05/main.go
+++ 2015/day-05/main.go
@@ -8,15 +8,21 @@ import (
 
 func main() {
 	var niceStrings int
+	var niceStrings2 int
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		if nice(scanner.Text()) {
 			niceStrings++
 		}
+
+		if nice2(scanner.Text()) {
+			niceStrings2++
+		}
 	}
 
 	fmt.Printf("Part 1: %d\n", niceStrings)
+	fmt.Printf("Part 2: %d\n", niceStrings2)
 }
 
 func nice(s string) bool {
@@ -56,4 +62,47 @@ func nice(s string) bool {
 	hasThreeVowels = vowelCount >= 3
 
 	return hasThreeVowels && hasDoubleLetter && !hasForbiddenString
+}
+
+func nice2(s string) bool {
+	return hasTwoNonOverlappingPairs(s) && hasRepeatedLetterWithOneInbetween(s)
+}
+
+func hasTwoNonOverlappingPairs(s string) bool {
+	pairs := map[string]struct{}{}
+
+	var (
+		lastR    rune
+		lastPair string
+	)
+
+	for n, r := range s {
+		if n != 0 {
+			pair := string([]rune{lastR, r})
+
+			if _, ok := pairs[pair]; ok {
+				return true
+			}
+
+			pairs[lastPair] = struct{}{}
+			lastPair = pair
+		}
+
+		lastR = r
+	}
+
+	return false
+}
+
+func hasRepeatedLetterWithOneInbetween(s string) bool {
+	var lastR, lastLastR rune
+
+	for _, r := range s {
+		if r == lastLastR {
+			return true
+		}
+		lastR, lastLastR = r, lastR
+	}
+
+	return false
 }
--- 2015/day-05/main_test.go
+++ 2015/day-05/main_test.go
@@ -18,3 +18,70 @@ func TestNice(t *testing.T) {
 		}
 	}
 }
+
+func TestNice2(t *testing.T) {
+	for n, tc := range []struct {
+		s    string
+		nice bool
+	}{
+		{"qjhvhtzxzqqjkmpb", true},
+		{"xxyxx", true},
+		{"uurcxstgmygtbstg", false},
+		{"ieodomkazucvgmuy", false},
+	} {
+		if got, want := nice2(tc.s), tc.nice; got != want {
+			t.Errorf("[%d] nice2(%q) = %t, want %t", n, tc.s, got, want)
+		}
+	}
+}
+
+func TestHasTwoNonOverlappingPairs(t *testing.T) {
+	for n, tc := range []struct {
+		s    string
+		want bool
+	}{
+		{"", false},
+		{"a", false},
+		{"aa", false},
+		{"aaa", false},
+		{"aaaa", true},
+		{"abab", true},
+		{"abba", false},
+		{"aa_aa", true},
+		{"ab_ab", true},
+		{"_aa_aa", true},
+		{"_ab_ab", true},
+		{"_abba", false},
+	} {
+		if got, want := hasTwoNonOverlappingPairs(tc.s), tc.want; got != want {
+			t.Errorf("[%d] hasNonOverlappingPair(%q) = %t, want %t", n, tc.s, got, want)
+		}
+	}
+}
+
+func TestHasRepeatedLetterWithOneInbetween(t *testing.T) {
+	for n, tc := range []struct {
+		s    string
+		want bool
+	}{
+		{"", false},
+		{"a", false},
+		{"aa", false},
+		{"aaa", true},
+		{"aba", true},
+		{"abba", false},
+		{"_aa", false},
+		{"_aaa", true},
+		{"_aba", true},
+		{"aa_", false},
+		{"aaa_", true},
+		{"aba_", true},
+		{"_aa_", false},
+		{"_aaa_", true},
+		{"_aba_", true},
+	} {
+		if got, want := hasRepeatedLetterWithOneInbetween(tc.s), tc.want; got != want {
+			t.Errorf("[%d] hasRepeatedLetterWithOneInbetween(%q) = %t, want %t", n, tc.s, got, want)
+		}
+	}
+}
