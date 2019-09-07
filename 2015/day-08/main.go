a8aad7d679a48161c0e40d5f659b3d2efa7b4e28
Johan Sageryd <j@1616.se>
2019-09-07 19:54:41 +0200

    2015 Day 8: Matchsticks; part 2

--- 2015/day-08/main.go
+++ 2015/day-08/main.go
@@ -11,18 +11,25 @@ func main() {
 	var (
 		totalLength         int
 		totalLengthUnquoted int
+		totalLengthRequoted int
 	)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		totalLength += len(scanner.Text())
 		totalLengthUnquoted += len(unquote(scanner.Text()))
+		totalLengthRequoted += len(quote(scanner.Text()))
 	}
 
 	fmt.Printf("Part 1: %d\n", totalLength-totalLengthUnquoted)
+	fmt.Printf("Part 2: %d\n", totalLengthRequoted-totalLength)
 }
 
 func unquote(s string) string {
 	s, _ = strconv.Unquote(s)
 	return s
+}
+
+func quote(s string) string {
+	return strconv.Quote(s)
 }
--- 2015/day-08/main_test.go
+++ 2015/day-08/main_test.go
@@ -19,3 +19,19 @@ func Test_Unquote(t *testing.T) {
 		}
 	}
 }
+
+func Test_Quote(t *testing.T) {
+	for n, tc := range []struct {
+		in  string
+		out string
+	}{
+		{`""`, `"\"\""`},
+		{`"abc"`, `"\"abc\""`},
+		{`"aaa\"aaa"`, `"\"aaa\\\"aaa\""`},
+		{`"\x27"`, `"\"\\x27\""`},
+	} {
+		if got, want := quote(tc.in), tc.out; got != want {
+			t.Errorf("[%d] got `%s`, want `%s`", n, got, want)
+		}
+	}
+}
