1b2dc5431f4bb0e44ee64444069c77ff9c0440b1
Johan Sageryd <j@1616.se>
2020-12-02 11:43:25 +0100

    2020 Day 2: Password Philosophy; part 2

--- 2020/day-02/main.go
+++ 2020/day-02/main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	var (
-		min, max int
+		n1, n2   int
 		letter   byte
 		password string
 	)
@@ -14,18 +14,24 @@ func main() {
 	format := "%d-%d %c: %s"
 
 	var validCount int
+	var validPart2Count int
 
 	for {
-		if _, err := fmt.Scanf(format, &min, &max, &letter, &password); err != nil {
+		if _, err := fmt.Scanf(format, &n1, &n2, &letter, &password); err != nil {
 			break
 		}
 
-		if valid(min, max, letter, password) {
+		if valid(n1, n2, letter, password) {
 			validCount++
+		}
+
+		if validPart2(n1, n2, letter, password) {
+			validPart2Count++
 		}
 	}
 
 	fmt.Printf("Part 1: %d\n", validCount)
+	fmt.Printf("Part 2: %d\n", validPart2Count)
 }
 
 func valid(min, max int, letter byte, password string) bool {
@@ -36,4 +42,10 @@ func valid(min, max int, letter byte, password string) bool {
 		}
 	}
 	return count >= min && count <= max
+}
+
+func validPart2(pos1, pos2 int, letter byte, password string) bool {
+	p1 := password[pos1-1] == letter
+	p2 := password[pos2-1] == letter
+	return (p1 || p2) && !(p1 && p2)
 }
--- 2020/day-02/main_test.go
+++ 2020/day-02/main_test.go
@@ -18,3 +18,20 @@ func TestValid(t *testing.T) {
 		}
 	}
 }
+
+func TestValidPart2(t *testing.T) {
+	for n, tc := range []struct {
+		pos1, pos2 int
+		letter     byte
+		password   string
+		valid      bool
+	}{
+		0: {1, 3, 'a', "abcde", true},
+		1: {1, 3, 'b', "cdefg", false},
+		2: {2, 9, 'c', "ccccccccc", false},
+	} {
+		if got, want := validPart2(tc.pos1, tc.pos2, tc.letter, tc.password), tc.valid; got != want {
+			t.Errorf("[%d] validPart2(%d, %d, %q, %q) = %t, want %t", n, tc.pos1, tc.pos2, tc.letter, tc.password, got, want)
+		}
+	}
+}
