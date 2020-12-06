15a64ebd0bc750a113676c23bb2ef93e958b3b3e
Johan Sageryd <j@1616.se>
2020-12-06 12:43:49 +0100

    2020 Day 6: Custom Customs; part 2

--- 2020/day-06/main.go
+++ 2020/day-06/main.go
@@ -23,6 +23,7 @@ func main() {
 	groups = append(groups, group)
 
 	fmt.Printf("Part 1: %d\n", part1(groups))
+	fmt.Printf("Part 2: %d\n", part2(groups))
 }
 
 func part1(groups [][]string) int {
@@ -32,6 +33,29 @@ func part1(groups [][]string) int {
 		for _, answers := range group {
 			for _, answer := range answers {
 				m[answer] = struct{}{}
+			}
+		}
+		sum += len(m)
+	}
+	return sum
+}
+
+func part2(groups [][]string) int {
+	var sum int
+	for _, group := range groups {
+		m := make(map[rune]struct{})
+		for _, answer := range group[0] {
+			m[answer] = struct{}{}
+		}
+		for _, answers := range group {
+			m2 := make(map[rune]struct{})
+			for _, answer := range answers {
+				m2[answer] = struct{}{}
+			}
+			for haveAnswer := range m {
+				if _, ok := m2[haveAnswer]; !ok {
+					delete(m, haveAnswer)
+				}
 			}
 		}
 		sum += len(m)
--- 2020/day-06/main_test.go
+++ 2020/day-06/main_test.go
@@ -2,16 +2,22 @@ package main
 
 import "testing"
 
-func TestPart1(t *testing.T) {
-	groups := [][]string{
-		{"abc"},
-		{"a", "b", "c"},
-		{"ab", "ac"},
-		{"a", "a", "a", "a"},
-		{"b"},
-	}
+var groups = [][]string{
+	{"abc"},
+	{"a", "b", "c"},
+	{"ab", "ac"},
+	{"a", "a", "a", "a"},
+	{"b"},
+}
 
+func TestPart1(t *testing.T) {
 	if got, want := part1(groups), 11; got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	if got, want := part2(groups), 6; got != want {
 		t.Errorf("got %d, want %d", got, want)
 	}
 }
