6432355d31dfbd3d08bda4a2482b5142d8985047
Johan Sageryd <j@1616.se>
2020-12-11 16:37:22 +0100

    2020 Day 11: Seating System; part 2

--- 2020/day-11/main.go
+++ 2020/day-11/main.go
@@ -14,13 +14,14 @@ func main() {
 		input = append(input, scanner.Text())
 	}
 
-	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 1: %d\n", run(input, step))
+	fmt.Printf("Part 2: %d\n", run(input, stepV2))
 }
 
-func part1(input []string) int {
+func run(input []string, stepFunc func(layout map[coord]byte) bool) int {
 	layout := makeLayoutMap(input)
 
-	for step(layout) {
+	for stepFunc(layout) {
 	}
 
 	var count int
@@ -74,6 +75,74 @@ func step(layout map[coord]byte) bool {
 			}
 		case '#':
 			if adjacentOccupiedSeats(k) >= 4 {
+				layout[k] = 'L'
+				modified = true
+			}
+		}
+	}
+
+	return modified
+}
+
+func stepV2(layout map[coord]byte) bool {
+	var modified bool
+	current := make(map[coord]byte, len(layout))
+
+	for k, v := range layout {
+		current[k] = v
+	}
+
+	walk := func(c coord, direction int) coord {
+		switch direction {
+		case 0: // north
+			return coord{c.x, c.y - 1}
+		case 1: // north-west
+			return coord{c.x - 1, c.y - 1}
+		case 2: // west
+			return coord{c.x - 1, c.y}
+		case 3: // south-west
+			return coord{c.x - 1, c.y + 1}
+		case 4: // south
+			return coord{c.x, c.y + 1}
+		case 5: // south-east
+			return coord{c.x + 1, c.y + 1}
+		case 6: // east
+			return coord{c.x + 1, c.y}
+		case 7: // north-east
+			return coord{c.x + 1, c.y - 1}
+		default:
+			panic("unknown direction")
+		}
+	}
+
+	occupiedSeatsInAnyDirection := func(c coord) int {
+		var count int
+		for d := 0; d < 8; d++ {
+			cc := c
+			for {
+				cc = walk(cc, d)
+				v, ok := current[cc]
+				if !ok || v == 'L' {
+					break
+				}
+				if v == '#' {
+					count++
+					break
+				}
+			}
+		}
+		return count
+	}
+
+	for k, v := range current {
+		switch v {
+		case 'L':
+			if occupiedSeatsInAnyDirection(k) == 0 {
+				layout[k] = '#'
+				modified = true
+			}
+		case '#':
+			if occupiedSeatsInAnyDirection(k) >= 5 {
 				layout[k] = 'L'
 				modified = true
 			}
--- 2020/day-11/main_test.go
+++ 2020/day-11/main_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestPart1(t *testing.T) {
+func TestRun(t *testing.T) {
 	input := []string{
 		"L.LL.LL.LL",
 		"LLLLLLL.LL",
@@ -19,9 +19,17 @@ func TestPart1(t *testing.T) {
 		"L.LLLLL.LL",
 	}
 
-	if got, want := part1(input), 37; got != want {
-		t.Errorf("got %d, want %d", got, want)
-	}
+	t.Run("Part 1", func(t *testing.T) {
+		if got, want := run(input, step), 37; got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+
+	t.Run("Part 2", func(t *testing.T) {
+		if got, want := run(input, stepV2), 26; got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
 }
 
 func TestStep(t *testing.T) {
@@ -120,6 +128,129 @@ func TestStep(t *testing.T) {
 		wantLayout := makeLayoutMap(next)
 
 		step(layout)
+
+		if fmt.Sprint(layout) != fmt.Sprint(wantLayout) {
+			t.Errorf("[%d] got %v,\nwant %v", i, layout, wantLayout)
+		}
+	}
+
+	if got, want := step(makeLayoutMap(steps[0])), true; got != want {
+		t.Errorf("[modified layout] got %t, want %t", got, want)
+	}
+
+	if got, want := step(makeLayoutMap(steps[len(steps)-1])), false; got != want {
+		t.Errorf("[unmodified layout] got %t, want %t", got, want)
+	}
+}
+
+func TestStepV2(t *testing.T) {
+	steps := [][]string{
+		{
+			"L.LL.LL.LL",
+			"LLLLLLL.LL",
+			"L.L.L..L..",
+			"LLLL.LL.LL",
+			"L.LL.LL.LL",
+			"L.LLLLL.LL",
+			"..L.L.....",
+			"LLLLLLLLLL",
+			"L.LLLLLL.L",
+			"L.LLLLL.LL",
+		},
+		{
+			"#.##.##.##",
+			"#######.##",
+			"#.#.#..#..",
+			"####.##.##",
+			"#.##.##.##",
+			"#.#####.##",
+			"..#.#.....",
+			"##########",
+			"#.######.#",
+			"#.#####.##",
+		},
+		{
+			"#.LL.LL.L#",
+			"#LLLLLL.LL",
+			"L.L.L..L..",
+			"LLLL.LL.LL",
+			"L.LL.LL.LL",
+			"L.LLLLL.LL",
+			"..L.L.....",
+			"LLLLLLLLL#",
+			"#.LLLLLL.L",
+			"#.LLLLL.L#",
+		},
+		{
+			"#.L#.##.L#",
+			"#L#####.LL",
+			"L.#.#..#..",
+			"##L#.##.##",
+			"#.##.#L.##",
+			"#.#####.#L",
+			"..#.#.....",
+			"LLL####LL#",
+			"#.L#####.L",
+			"#.L####.L#",
+		},
+		{
+			"#.L#.L#.L#",
+			"#LLLLLL.LL",
+			"L.L.L..#..",
+			"##LL.LL.L#",
+			"L.LL.LL.L#",
+			"#.LLLLL.LL",
+			"..L.L.....",
+			"LLLLLLLLL#",
+			"#.LLLLL#.L",
+			"#.L#LL#.L#",
+		},
+		{
+			"#.L#.L#.L#",
+			"#LLLLLL.LL",
+			"L.L.L..#..",
+			"##L#.#L.L#",
+			"L.L#.#L.L#",
+			"#.L####.LL",
+			"..#.#.....",
+			"LLL###LLL#",
+			"#.LLLLL#.L",
+			"#.L#LL#.L#",
+		},
+		{
+			"#.L#.L#.L#",
+			"#LLLLLL.LL",
+			"L.L.L..#..",
+			"##L#.#L.L#",
+			"L.L#.LL.L#",
+			"#.LLLL#.LL",
+			"..#.L.....",
+			"LLL###LLL#",
+			"#.LLLLL#.L",
+			"#.L#LL#.L#",
+		},
+		{
+			"#.L#.L#.L#",
+			"#LLLLLL.LL",
+			"L.L.L..#..",
+			"##L#.#L.L#",
+			"L.L#.LL.L#",
+			"#.LLLL#.LL",
+			"..#.L.....",
+			"LLL###LLL#",
+			"#.LLLLL#.L",
+			"#.L#LL#.L#",
+		},
+	}
+
+	for i := 0; i < len(steps)-1; i++ {
+		input := steps[i]
+		next := steps[i+1]
+
+		layout := makeLayoutMap(input)
+		wantLayout := makeLayoutMap(next)
+
+		stepV2(layout)
 
 		if fmt.Sprint(layout) != fmt.Sprint(wantLayout) {
 			t.Errorf("[%d] got %v,\nwant %v", i, layout, wantLayout)
