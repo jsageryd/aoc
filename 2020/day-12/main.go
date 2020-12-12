e658c6f45826440deb51bb2a3402ca5da9738f6a
Johan Sageryd <j@1616.se>
2020-12-12 18:33:49 +0100

    2020 Day 12: Rain Risk; part 2

--- 2020/day-12/main.go
+++ 2020/day-12/main.go
@@ -16,11 +16,18 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
 func part1(input []string) int {
 	s := newShip()
 	navigate(s, input)
+	return abs(s.x) + abs(s.y)
+}
+
+func part2(input []string) int {
+	s := newShip()
+	navigateWithWaypoint(s, input)
 	return abs(s.x) + abs(s.y)
 }
 
@@ -71,6 +78,61 @@ func navigate(s *ship, instructions []string) {
 			turn(value)
 		case 'F':
 			move(s.heading, value)
+		}
+	}
+}
+
+func navigateWithWaypoint(s *ship, instructions []string) {
+	waypoint := struct {
+		dx, dy int
+	}{10, -1}
+
+	moveWaypoint := func(heading byte, distance int) {
+		switch heading {
+		case 'N':
+			waypoint.dy -= distance
+		case 'E':
+			waypoint.dx += distance
+		case 'S':
+			waypoint.dy += distance
+		case 'W':
+			waypoint.dx -= distance
+		}
+	}
+
+	rotateWaypoint := func(degrees int) {
+		const cos90 = 0
+		const sin90 = 1
+
+		for degrees < 0 {
+			degrees += 360
+		}
+
+		for i := 0; i < (degrees/90)%4; i++ {
+			newX := waypoint.dx*cos90 - waypoint.dy*sin90
+			newY := waypoint.dx*sin90 + waypoint.dy*cos90
+			waypoint.dx, waypoint.dy = newX, newY
+		}
+
+		// https://en.wikipedia.org/wiki/Rotation_matrix#In_two_dimensions
+	}
+
+	for _, inst := range instructions {
+		var action byte
+		var value int
+
+		fmt.Sscanf(inst, "%c%d", &action, &value)
+
+		switch action {
+		case 'N', 'E', 'S', 'W':
+			moveWaypoint(action, value)
+		case 'L':
+			rotateWaypoint(-value)
+		case 'R':
+			rotateWaypoint(value)
+		case 'F':
+			s.x += value * waypoint.dx
+			s.y += value * waypoint.dy
 		}
 	}
 }
--- 2020/day-12/main_test.go
+++ 2020/day-12/main_test.go
@@ -2,16 +2,22 @@ package main
 
 import "testing"
 
-func TestPart1(t *testing.T) {
-	input := []string{
-		"F10",
-		"N3",
-		"F7",
-		"R90",
-		"F11",
-	}
+var input = []string{
+	"F10",
+	"N3",
+	"F7",
+	"R90",
+	"F11",
+}
 
+func TestPart1(t *testing.T) {
 	if got, want := part1(input), 25; got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	if got, want := part2(input), 286; got != want {
 		t.Errorf("got %d, want %d", got, want)
 	}
 }
