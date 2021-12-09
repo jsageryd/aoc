d18f9ad6706882449fdbf74a814be1053a3c7d42
Johan Sageryd <j@1616.se>
2021-12-09 16:24:55 +0100

    2021 Day 9: Smoke Basin; part 2

--- 2021/day-09/main.go
+++ 2021/day-09/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -17,6 +18,7 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
 func part1(input []string) int {
@@ -35,6 +37,30 @@ func part1(input []string) int {
 	}
 
 	return riskLevelSum
+}
+
+func part2(input []string) int {
+	if len(input) == 0 {
+		return 0
+	}
+
+	heightMap := parse(input)
+
+	basinSizes := findBasinSizes(heightMap)
+
+	if len(basinSizes) < 3 {
+		log.Fatalf("found only %d basins", len(basinSizes))
+	}
+
+	sort.Ints(basinSizes)
+
+	prod := 1
+
+	for _, size := range basinSizes[len(basinSizes)-3:] {
+		prod *= size
+	}
+
+	return prod
 }
 
 func parse(input []string) map[coord]int {
@@ -68,6 +94,70 @@ func riskLevel(x, y int, heightMap map[coord]int) int {
 	}
 
 	return height + 1
+}
+
+func findLowPoints(heightMap map[coord]int) []coord {
+	var lowPoints []coord
+
+next:
+	for c := range heightMap {
+		height := heightMap[c]
+
+		for _, adjCoord := range []coord{
+			{c.x, c.y - 1},
+			{c.x - 1, c.y},
+			{c.x, c.y + 1},
+			{c.x + 1, c.y},
+		} {
+			if adjHeight, ok := heightMap[adjCoord]; ok {
+				if adjHeight <= height {
+					continue next
+				}
+			}
+		}
+
+		lowPoints = append(lowPoints, c)
+	}
+
+	return lowPoints
+}
+
+func findBasinSizes(heightMap map[coord]int) []int {
+	var basinSizes []int
+
+	var maxX, maxY int
+	for c := range heightMap {
+		if c.x > maxX {
+			maxX = c.x
+		}
+		if c.y > maxY {
+			maxY = c.y
+		}
+	}
+
+	for _, c := range findLowPoints(heightMap) {
+		seen := make(map[coord]bool)
+		var floodFind func(c coord)
+		floodFind = func(c coord) {
+			if c.x < 0 || c.x > maxX || c.y < 0 || c.y > maxY ||
+				seen[c] || heightMap[c] == 9 {
+				return
+			}
+			seen[c] = true
+			for _, adjC := range []coord{
+				{c.x, c.y - 1},
+				{c.x - 1, c.y},
+				{c.x, c.y + 1},
+				{c.x + 1, c.y},
+			} {
+				floodFind(adjC)
+			}
+		}
+		floodFind(c)
+		basinSizes = append(basinSizes, len(seen))
+	}
+
+	return basinSizes
 }
 
 type coord struct {
--- 2021/day-09/main_test.go
+++ 2021/day-09/main_test.go
@@ -1,17 +1,42 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
+
+var input = []string{
+	"2199943210",
+	"3987894921",
+	"9856789892",
+	"8767896789",
+	"9899965678",
+}
 
 func TestPart1(t *testing.T) {
-	input := []string{
-		"2199943210",
-		"3987894921",
-		"9856789892",
-		"8767896789",
-		"9899965678",
-	}
-
 	if got, want := part1(input), 15; got != want {
 		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	if got, want := part2(input), 1134; got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestFindLowPoints(t *testing.T) {
+	gotLowPoints := findLowPoints(parse(input))
+
+	sort.Slice(gotLowPoints, func(i, j int) bool {
+		ic, jc := gotLowPoints[i], gotLowPoints[j]
+		return ic.y*10+ic.x < jc.y*10+jc.x
+	})
+
+	wantLowPoints := []coord{{1, 0}, {9, 0}, {2, 2}, {6, 4}}
+
+	if fmt.Sprint(gotLowPoints) != fmt.Sprint(wantLowPoints) {
+		t.Errorf("got %v, want %v", gotLowPoints, wantLowPoints)
 	}
 }
