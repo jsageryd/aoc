e08e9e9a29c72b851885bd79a6ac89f158231dac
Johan Sageryd <j@1616.se>
2022-12-06 06:24:35 +0100

    Simplify

--- 2022/day-06/main.go
+++ 2022/day-06/main.go
@@ -9,42 +9,17 @@ func main() {
 
 	fmt.Scanln(&input)
 
-	fmt.Printf("Part 1: %d\n", part1(input))
-	fmt.Printf("Part 2: %d\n", part2(input))
+	fmt.Printf("Part 1: %d\n", startOfMessage(input, 4))
+	fmt.Printf("Part 2: %d\n", startOfMessage(input, 14))
 }
 
-func part1(input string) int {
-	for n := 3; n < len(input); n++ {
-		m := map[byte]struct{}{}
-		m[input[n]] = struct{}{}
-		m[input[n-1]] = struct{}{}
-		m[input[n-2]] = struct{}{}
-		m[input[n-3]] = struct{}{}
-		if len(m) == 4 {
-			return n + 1
+func startOfMessage(input string, markerLength int) int {
+	for n := markerLength - 1; n < len(input); n++ {
+		m := make(map[byte]struct{})
+		for i := 0; i < markerLength; i++ {
+			m[input[n-i]] = struct{}{}
 		}
-	}
-	return 0
-}
-
-func part2(input string) int {
-	for n := 13; n < len(input); n++ {
-		m := map[byte]struct{}{}
-		m[input[n]] = struct{}{}
-		m[input[n-1]] = struct{}{}
-		m[input[n-2]] = struct{}{}
-		m[input[n-3]] = struct{}{}
-		m[input[n-4]] = struct{}{}
-		m[input[n-5]] = struct{}{}
-		m[input[n-6]] = struct{}{}
-		m[input[n-7]] = struct{}{}
-		m[input[n-8]] = struct{}{}
-		m[input[n-9]] = struct{}{}
-		m[input[n-10]] = struct{}{}
-		m[input[n-11]] = struct{}{}
-		m[input[n-12]] = struct{}{}
-		m[input[n-13]] = struct{}{}
-		if len(m) == 14 {
+		if len(m) == markerLength {
 			return n + 1
 		}
 	}
--- 2022/day-06/main_test.go
+++ 2022/day-06/main_test.go
@@ -2,34 +2,23 @@ package main
 
 import "testing"
 
-func TestPart1(t *testing.T) {
+func TestStartOfMessage(t *testing.T) {
 	for n, tc := range []struct {
-		input string
-		start int
+		input        string
+		markerLength int
+		start        int
 	}{
-		{"bvwbjplbgvbhsrlpgdmjqwftvncz", 5},
-		{"nppdvjthqldpwncqszvftbrmjlhg", 6},
-		{"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10},
-		{"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11},
+		{"bvwbjplbgvbhsrlpgdmjqwftvncz", 4, 5},
+		{"nppdvjthqldpwncqszvftbrmjlhg", 4, 6},
+		{"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4, 10},
+		{"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4, 11},
+		{"mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14, 19},
+		{"bvwbjplbgvbhsrlpgdmjqwftvncz", 14, 23},
+		{"nppdvjthqldpwncqszvftbrmjlhg", 14, 23},
+		{"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14, 29},
+		{"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14, 26},
 	} {
-		if got, want := part1(tc.input), tc.start; got != want {
-			t.Errorf("[%d] got %d, want %d", n, got, want)
-		}
-	}
-}
-
-func TestPart2(t *testing.T) {
-	for n, tc := range []struct {
-		input string
-		start int
-	}{
-		{"mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19},
-		{"bvwbjplbgvbhsrlpgdmjqwftvncz", 23},
-		{"nppdvjthqldpwncqszvftbrmjlhg", 23},
-		{"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29},
-		{"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26},
-	} {
-		if got, want := part2(tc.input), tc.start; got != want {
+		if got, want := startOfMessage(tc.input, tc.markerLength), tc.start; got != want {
 			t.Errorf("[%d] got %d, want %d", n, got, want)
 		}
 	}
