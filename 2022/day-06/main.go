7f3aae916af0a2423839456cc6c209d4bcf429d9
Johan Sageryd <j@1616.se>
2022-12-06 06:15:17 +0100

    2022 Day 6: Tuning Trouble; part 2
    Just followed the same pattern from part 1.

--- 2022/day-06/main.go
+++ 2022/day-06/main.go
@@ -10,6 +10,7 @@ func main() {
 	fmt.Scanln(&input)
 
 	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
 func part1(input string) int {
@@ -20,6 +21,30 @@ func part1(input string) int {
 		m[input[n-2]] = struct{}{}
 		m[input[n-3]] = struct{}{}
 		if len(m) == 4 {
+			return n + 1
+		}
+	}
+	return 0
+}
+
+func part2(input string) int {
+	for n := 13; n < len(input); n++ {
+		m := map[byte]struct{}{}
+		m[input[n]] = struct{}{}
+		m[input[n-1]] = struct{}{}
+		m[input[n-2]] = struct{}{}
+		m[input[n-3]] = struct{}{}
+		m[input[n-4]] = struct{}{}
+		m[input[n-5]] = struct{}{}
+		m[input[n-6]] = struct{}{}
+		m[input[n-7]] = struct{}{}
+		m[input[n-8]] = struct{}{}
+		m[input[n-9]] = struct{}{}
+		m[input[n-10]] = struct{}{}
+		m[input[n-11]] = struct{}{}
+		m[input[n-12]] = struct{}{}
+		m[input[n-13]] = struct{}{}
+		if len(m) == 14 {
 			return n + 1
 		}
 	}
