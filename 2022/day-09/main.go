0e4919506b9b580a2ac61eb33d964b802a9aac61
Johan Sageryd <j@1616.se>
2022-12-09 10:09:13 +0100

    2022 Day 9: Rope Bridge; part 2

--- 2022/day-09/main.go
+++ 2022/day-09/main.go
@@ -16,6 +16,7 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
 func part1(input []string) int {
@@ -29,6 +30,25 @@ func part1(input []string) int {
 			h.move(string(motion[0]))
 			t.follow(h)
 			tailLocations[t] = struct{}{}
+		}
+	}
+
+	return len(tailLocations)
+}
+
+func part2(input []string) int {
+	tailLocations := make(map[coord]struct{})
+
+	knots := make([]coord, 10)
+
+	for _, motion := range input {
+		dist, _ := strconv.Atoi(motion[2:])
+		for n := 0; n < dist; n++ {
+			knots[0].move(string(motion[0]))
+			for m := 1; m < len(knots); m++ {
+				knots[m].follow(knots[m-1])
+			}
+			tailLocations[knots[9]] = struct{}{}
 		}
 	}
 
--- 2022/day-09/main_test.go
+++ 2022/day-09/main_test.go
@@ -19,6 +19,20 @@ func TestPart1(t *testing.T) {
 	}
 }
 
+func TestPart2(t *testing.T) {
+	for n, tc := range []struct {
+		input []string
+		want  int
+	}{
+		{[]string{"R 4", "U 4", "L 3", "D 1", "R 4", "D 1", "L 5", "R 2"}, 1},
+		{[]string{"R 5", "U 8", "L 8", "D 3", "R 17", "D 10", "L 25", "U 20"}, 36},
+	} {
+		if got, want := part2(tc.input), tc.want; got != want {
+			t.Errorf("[%d] got %d, want %d", n, got, want)
+		}
+	}
+}
+
 func TestCoord_Move(t *testing.T) {
 	for n, tc := range []struct {
 		c         coord
