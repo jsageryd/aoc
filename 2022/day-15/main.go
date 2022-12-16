4f6924fdac77b4b963f60a7fa075c81ed10f079a
Johan Sageryd <j@1616.se>
2022-12-16 18:26:31 +0100

    Optimize part 2
    This reduces the number of coordinates to check by only checking those
    along the perimeter of each sensor's radius.
    
    I tried implementing something more similar to part 1 where I would
    still loop over all coordinates within the given limit but skip over
    blocks of coordinates known to have no beacons, but I couldn't get the
    offsets right so gave up on that and went with this slightly more
    creative solution instead.

--- 2022/day-15/main.go
+++ 2022/day-15/main.go
@@ -43,24 +43,82 @@ func part1(input []string, row int) int {
 func part2(input []string, max int) int {
 	m := parseInput(input)
 
+	// loopSensor loops around the sensor at the given radius, calling f for each
+	// coordinate it visits, as long as f returns true.
+	loopSensor := func(sensor coord, radius int, f func(c coord) bool) {
+		// Start at 12 o'clock
+		c := coord{
+			x: sensor.x,
+			y: sensor.y - radius - 1,
+		}
+		if !f(c) {
+			return
+		}
+
+		// Counter-clockwise top-left quadrant
+		for c.y < sensor.y {
+			c.x--
+			c.y++
+			if !f(c) {
+				return
+			}
+		}
+
+		// Bottom-left quadrant
+		for c.y < sensor.y+radius+1 {
+			c.x++
+			c.y++
+			if !f(c) {
+				return
+			}
+		}
+
+		// Bottom-right quadrant
+		for c.y > sensor.y {
+			c.x++
+			c.y--
+			if !f(c) {
+				return
+			}
+		}
+
+		// Top-right quadrant
+		for c.y > sensor.y-radius {
+			c.x--
+			c.y--
+			if !f(c) {
+				return
+			}
+		}
+	}
+
 	var missingBeacon coord
 
 	md := manhattanDistance
 
-	for y := 0; y <= max; y++ {
-		for x := 0; x <= max; x++ {
-			var withinRange bool
-			for sensor, beacon := range m {
-				if md(sensor, beacon) >= md(sensor, coord{x, y}) {
-					withinRange = true
-					break
+	for sensor, beacon := range m {
+		loopSensor(sensor, md(sensor, beacon), func(c coord) bool {
+			// skip this coordinate if it's outside of the given constraints.
+			if c.x < 0 || c.x > max || c.y < 0 || c.y > max {
+				return true
+			}
+
+			for otherSensor, otherBeacon := range m {
+				if otherSensor != sensor {
+					if md(otherSensor, otherBeacon) >= md(otherSensor, c) {
+						// This coordinate is within the radius of another sensor, so the
+						// missing beacon cannot be here. Continue checking the next
+						// coordinate.
+						return true
+					}
 				}
 			}
-			if !withinRange {
-				missingBeacon = coord{x, y}
-				break
-			}
-		}
+
+			// c was not within the radius of any other sensor, so this must be the
+			// coordinate of the missing beacon.
+			missingBeacon = c
+			return false
+		})
 	}
 
 	return missingBeacon.x*4000000 + missingBeacon.y
