c23ac973c9ae65531153dd2666304e99f07c48f8
Johan Sageryd <j@1616.se>
2023-02-05 19:24:20 +0100

    2015 Day 20: Infinite Elves and Infinite Houses; part 2

--- 2015/day-20/main.go
+++ 2015/day-20/main.go
@@ -10,6 +10,7 @@ func main() {
 	fmt.Scanln(&input)
 
 	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
 func part1(input int) int {
@@ -22,6 +23,28 @@ func part1(input int) int {
 
 		// p is house -> presents so p[e] looks like an error, but e is already
 		// looping from 1 to len(p), so we can re-use that.
+		if p[e] >= input {
+			return e
+		}
+	}
+
+	return 0
+}
+
+func part2(input int) int {
+	p := make([]int, input) // house -> presents
+
+	for e := 1; e < len(p); e++ {
+		for n := 0; n < 50; n++ {
+			h := n * e
+
+			if h > len(p)-1 {
+				break
+			}
+
+			p[h] += 11 * e
+		}
+
 		if p[e] >= input {
 			return e
 		}
--- 2015/day-20/main_test.go
+++ 2015/day-20/main_test.go
@@ -34,3 +34,34 @@ func TestPart1(t *testing.T) {
 		}
 	}
 }
+
+func TestPart2(t *testing.T) {
+	/*
+	   House 1 got 11 presents.
+	   House 2 got 33 presents.
+	   House 3 got 44 presents.
+	   House 4 got 77 presents.
+	   House 5 got 66 presents.
+	   House 6 got 132 presents.
+	   House 7 got 88 presents.
+	   House 8 got 165 presents.
+	   House 9 got 143 presents.
+	*/
+
+	for n, tc := range []struct {
+		input int
+		want  int
+	}{
+		{11, 1},
+		{22, 2},
+		{33, 2},
+		{66, 4},
+		{88, 6},
+		{143, 8},
+		{165, 8},
+	} {
+		if got, want := part2(tc.input), tc.want; got != want {
+			t.Errorf("[%d] got %d, want %d", n, got, want)
+		}
+	}
+}
