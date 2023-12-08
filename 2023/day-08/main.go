e9e8350731b44d3182087312080807e19e98881b
Johan Sageryd <j@1616.se>
2023-12-08 09:13:30 +0100

    2023 Day 8: Haunted Wasteland; part 2
    The test passes but this is too slow for the real input.

--- 2023/day-08/main.go
+++ 2023/day-08/main.go
@@ -14,6 +14,7 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
 func part1(input []string) int {
@@ -33,6 +34,51 @@ func part1(input []string) int {
 
 	for cur != "ZZZ" {
 		cur = m[cur][inst[steps%len(inst)]]
+		steps++
+	}
+
+	return steps
+}
+
+func part2(input []string) int {
+	m := make(map[string]map[byte]string) // AAA -> {L: BBB, R: CCC}
+
+	for _, line := range input[2:] {
+		m[line[0:3]] = map[byte]string{
+			'L': line[7:10],
+			'R': line[12:15],
+		}
+	}
+
+	var curs []string
+
+	for _, line := range input[2:] {
+		if line[2] == 'A' {
+			curs = append(curs, line[0:3])
+		}
+	}
+
+	var steps int
+	var arrived bool
+
+	inst := input[0]
+
+	for {
+		arrived = true
+		for n := range curs {
+			if curs[n][2] != 'Z' {
+				arrived = false
+			}
+		}
+
+		if arrived {
+			break
+		}
+
+		for n := range curs {
+			curs[n] = m[curs[n]][inst[steps%len(inst)]]
+		}
+
 		steps++
 	}
 
--- 2023/day-08/main_test.go
+++ 2023/day-08/main_test.go
@@ -37,3 +37,22 @@ func TestPart1(t *testing.T) {
 		}
 	}
 }
+
+func TestPart2(t *testing.T) {
+	input := []string{
+		"LR",
+		"",
+		"11A = (11B, XXX)",
+		"11B = (XXX, 11Z)",
+		"11Z = (11B, XXX)",
+		"22A = (22B, XXX)",
+		"22B = (22C, 22C)",
+		"22C = (22Z, 22Z)",
+		"22Z = (22B, 22B)",
+		"XXX = (XXX, XXX)",
+	}
+
+	if got, want := part2(input), 6; got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
