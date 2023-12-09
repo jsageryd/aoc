8bc6e4e7bfe3bc3caf25b044147cf5a2e9bfcf3c
Johan Sageryd <j@1616.se>
2023-12-09 10:51:57 +0100

    2023 Day 9: Mirage Maintenance; part 2

--- 2023/day-09/main.go
+++ 2023/day-09/main.go
@@ -16,6 +16,7 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
 func part1(input []string) int {
@@ -23,6 +24,16 @@ func part1(input []string) int {
 
 	for _, line := range input {
 		sum += nextValue(line)
+	}
+
+	return sum
+}
+
+func part2(input []string) int {
+	var sum int
+
+	for _, line := range input {
+		sum += prevValue(line)
 	}
 
 	return sum
@@ -48,6 +59,28 @@ func nextValue(line string) int {
 	}
 
 	return seqs[0][len(seqs[0])-1]
+}
+
+func prevValue(line string) int {
+	seqs := [][]int{ints(line)}
+
+	for {
+		var seq []int
+		lastSeq := seqs[len(seqs)-1]
+		for i := 1; i < len(lastSeq); i++ {
+			seq = append(seq, lastSeq[i]-lastSeq[i-1])
+		}
+		if len(seq) < 1 {
+			break
+		}
+		seqs = append(seqs, seq)
+	}
+
+	for n := len(seqs) - 2; n >= 0; n-- {
+		seqs[n] = append([]int{seqs[n][0] - seqs[n+1][0]}, seqs[n]...)
+	}
+
+	return seqs[0][0]
 }
 
 func ints(s string) []int {
--- 2023/day-09/main_test.go
+++ 2023/day-09/main_test.go
@@ -14,6 +14,18 @@ func TestPart1(t *testing.T) {
 	}
 }
 
+func TestPart2(t *testing.T) {
+	input := []string{
+		"0 3 6 9 12 15",
+		"1 3 6 10 15 21",
+		"10 13 16 21 30 45",
+	}
+
+	if got, want := part2(input), 2; got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
 func TestNextValue(t *testing.T) {
 	for n, tc := range []struct {
 		line string
@@ -24,6 +36,21 @@ func TestNextValue(t *testing.T) {
 		{"10 13 16 21 30 45", 68},
 	} {
 		if got, want := nextValue(tc.line), tc.want; got != want {
+			t.Errorf("[%d] got %d, want %d", n, got, want)
+		}
+	}
+}
+
+func TestPrevValue(t *testing.T) {
+	for n, tc := range []struct {
+		line string
+		want int
+	}{
+		{"0 3 6 9 12 15", -3},
+		{"1 3 6 10 15 21", 0},
+		{"10 13 16 21 30 45", 5},
+	} {
+		if got, want := prevValue(tc.line), tc.want; got != want {
 			t.Errorf("[%d] got %d, want %d", n, got, want)
 		}
 	}
