7ba998f877480257335992b80a3d8b1331e0a4e9
Johan Sageryd <j@1616.se>
2024-12-03 08:09:06 +0100

    2024 Day 3: Mull It Over; part 2

--- 2024/day-03/main.go
+++ 2024/day-03/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"cmp"
 	"fmt"
 	"os"
 	"regexp"
@@ -16,6 +17,7 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
 func part1(input []string) int {
@@ -29,6 +31,34 @@ func part1(input []string) int {
 			b, _ := strconv.Atoi(match[2])
 
 			sum += a * b
+		}
+	}
+
+	return sum
+}
+
+func part2(input []string) int {
+	var sum int
+
+	allRe := regexp.MustCompile(`(do)\(\)|(don't)\(\)|(mul)\((\d{1,3}),(\d{1,3})\)`)
+
+	mulEnabled := true
+
+	for n := range input {
+		for _, match := range allRe.FindAllStringSubmatch(input[n], -1) {
+			switch cmp.Or(match[1], match[2], match[3]) {
+			case "do":
+				mulEnabled = true
+			case "don't":
+				mulEnabled = false
+			case "mul":
+				if mulEnabled {
+					a, _ := strconv.Atoi(match[4])
+					b, _ := strconv.Atoi(match[5])
+
+					sum += a * b
+				}
+			}
 		}
 	}
 
--- 2024/day-03/main_test.go
+++ 2024/day-03/main_test.go
@@ -11,3 +11,13 @@ func TestPart1(t *testing.T) {
 		t.Errorf("got %d, want %d", got, want)
 	}
 }
+
+func TestPart2(t *testing.T) {
+	input := []string{
+		"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+	}
+
+	if got, want := part2(input), 48; got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
