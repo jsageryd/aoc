b4992a58fb457903a379315341a045575e2948cd
Johan Sageryd <j@1616.se>
2024-12-07 12:30:37 +0100

    2024 Day 7: Bridge Repair; part 2

--- 2024/day-07/main.go
+++ 2024/day-07/main.go
@@ -17,6 +17,7 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
 func part1(input []string) int {
@@ -26,6 +27,22 @@ func part1(input []string) int {
 
 	for _, t := range tests {
 		results := calculate(t.numbers)
+
+		if slices.Contains(results, t.value) {
+			sum += t.value
+		}
+	}
+
+	return sum
+}
+
+func part2(input []string) int {
+	var sum int
+
+	tests := parse(input)
+
+	for _, t := range tests {
+		results := calculate2(t.numbers)
 
 		if slices.Contains(results, t.value) {
 			sum += t.value
@@ -48,6 +65,29 @@ func calculate(numbers []int) []int {
 
 		rec(acc+numbers[0], numbers[1:])
 		rec(acc*numbers[0], numbers[1:])
+	}
+
+	rec(numbers[0], numbers[1:])
+
+	return res
+}
+
+func calculate2(numbers []int) []int {
+	var res []int
+
+	var rec func(acc int, numbers []int)
+
+	rec = func(acc int, numbers []int) {
+		if len(numbers) == 0 {
+			res = append(res, acc)
+			return
+		}
+
+		rec(acc+numbers[0], numbers[1:])
+		rec(acc*numbers[0], numbers[1:])
+
+		n, _ := strconv.Atoi(strconv.Itoa(acc) + strconv.Itoa(numbers[0]))
+		rec(n, numbers[1:])
 	}
 
 	rec(numbers[0], numbers[1:])
--- 2024/day-07/main_test.go
+++ 2024/day-07/main_test.go
@@ -4,20 +4,26 @@ import (
 	"testing"
 )
 
-func TestPart1(t *testing.T) {
-	input := []string{
-		"190: 10 19",
-		"3267: 81 40 27",
-		"83: 17 5",
-		"156: 15 6",
-		"7290: 6 8 6 15",
-		"161011: 16 10 13",
-		"192: 17 8 14",
-		"21037: 9 7 18 13",
-		"292: 11 6 16 20",
-	}
+var input = []string{
+	"190: 10 19",
+	"3267: 81 40 27",
+	"83: 17 5",
+	"156: 15 6",
+	"7290: 6 8 6 15",
+	"161011: 16 10 13",
+	"192: 17 8 14",
+	"21037: 9 7 18 13",
+	"292: 11 6 16 20",
+}
 
+func TestPart1(t *testing.T) {
 	if got, want := part1(input), 3749; got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	if got, want := part2(input), 11387; got != want {
 		t.Errorf("got %d, want %d", got, want)
 	}
 }
