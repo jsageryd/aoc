b6b63aac7896b9194f8a0c738a226212af4f5d6c
Johan Sageryd <j@1616.se>
2024-12-08 08:24:24 +0100

    2024 Day 8: Resonant Collinearity; part 2

--- 2024/day-08/main.go
+++ 2024/day-08/main.go
@@ -14,6 +14,7 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
 func part1(input []string) int {
@@ -46,6 +47,41 @@ func part1(input []string) int {
 				if antinode.x >= 0 && antinode.x < len(input[0]) &&
 					antinode.y >= 0 && antinode.y < len(input) {
 					antinodes[antinode] = struct{}{}
+				}
+			}
+		}
+	}
+
+	return len(antinodes)
+}
+
+func part2(input []string) int {
+	antennas := make(map[byte][]Coord)
+
+	for y := range input {
+		for x := range input[y] {
+			if input[y][x] == '.' {
+				continue
+			}
+
+			antennas[input[y][x]] = append(antennas[input[y][x]], Coord{x, y})
+		}
+	}
+
+	antinodes := make(map[Coord]struct{})
+
+	for _, locations := range antennas {
+		for _, loc := range locations {
+			for _, loc2 := range locations {
+				if loc == loc2 {
+					continue
+				}
+
+				dx := loc.x - loc2.x
+				dy := loc.y - loc2.y
+
+				for x, y := loc.x, loc.y; 0 <= x && x < len(input[0]) && 0 <= y && y < len(input); x, y = x+dx, y+dy {
+					antinodes[Coord{x, y}] = struct{}{}
 				}
 			}
 		}
