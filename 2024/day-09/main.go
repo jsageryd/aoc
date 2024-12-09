6a26217c79fcf5bcb7f95b0d8ac3f07bfdf0202d
Johan Sageryd <j@1616.se>
2024-12-09 16:20:22 +0100

    2024 Day 9: Disk Fragmenter; part 2

--- 2024/day-09/main.go
+++ 2024/day-09/main.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"slices"
 )
 
 func main() {
 	input, _ := io.ReadAll(os.Stdin)
 
 	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
 func part1(input []byte) int {
@@ -47,6 +49,60 @@ func part1(input []byte) int {
 					N -= 2
 					fileID = N / 2
 				}
+			}
+		}
+	}
+
+	return sum
+}
+
+func part2(input []byte) int {
+	type file struct {
+		id   int
+		size int
+	}
+
+	fileMap := make(map[int][]file)
+
+	ints := parse(input)
+	origInts := slices.Clone(ints)
+
+	for N := len(ints) - 1; N >= 0; N -= 2 {
+		fileID := N / 2
+
+		for n := 1; n < N; n += 2 {
+			if ints[n] >= ints[N] {
+				fileMap[n] = append(fileMap[n], file{fileID, ints[N]})
+				ints[n] -= ints[N]
+				ints[N] = 0
+				break
+			}
+		}
+	}
+
+	for n := 0; n < len(ints); n += 2 {
+		fileMap[n] = []file{{n / 2, ints[n]}}
+	}
+
+	var sum int
+
+	var pos int
+
+	for n := range ints {
+		if files, ok := fileMap[n]; ok {
+			for _, f := range files {
+				for range f.size {
+					sum += pos * f.id
+					pos++
+				}
+			}
+		}
+
+		if n%2 != 0 {
+			pos += ints[n]
+		} else {
+			if ints[n] == 0 {
+				pos += origInts[n]
 			}
 		}
 	}
--- 2024/day-09/main_test.go
+++ 2024/day-09/main_test.go
@@ -4,10 +4,16 @@ import (
 	"testing"
 )
 
-func TestPart1(t *testing.T) {
-	input := []byte("2333133121414131402\n")
+var input = []byte("2333133121414131402\n")
 
+func TestPart1(t *testing.T) {
 	if got, want := part1(input), 1928; got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	if got, want := part2(input), 2858; got != want {
 		t.Errorf("got %d, want %d", got, want)
 	}
 }
