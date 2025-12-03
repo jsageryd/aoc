8204765ef637558efdc3158f674d82d1328ddad1
Johan Sageryd <j@1616.se>
2025-12-03 17:22:31 +0100

    2025 Day 3: Lobby; part 2

--- 2025/day-03/main.go
+++ 2025/day-03/main.go
@@ -14,6 +14,7 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
 func part1(input []string) int {
@@ -48,4 +49,61 @@ func part1(input []string) int {
 	}
 
 	return sum
+}
+
+func part2(input []string) int {
+	var sum int
+
+	for _, line := range input {
+		bank := make([]int, len(line))
+
+		for n := range line {
+			bank[n] = int(line[n]) - '0'
+		}
+
+		comb := maxComb(bank, 12)
+
+		var total int
+
+		m := 1
+
+		for n := len(comb) - 1; n >= 0; n-- {
+			total += comb[n] * m
+			m *= 10
+		}
+
+		sum += total
+	}
+
+	return sum
+}
+
+// maxComb picks k elements from digits until it finds the combination with the
+// highest numeric value formed by the picked digits.
+func maxComb(digits []int, k int) []int {
+	comb := make([]int, k)
+
+	var rec func(dd []int, cc []int) []int
+
+	rec = func(dd []int, cc []int) []int {
+		var maxHead int
+
+		for n := 0; n <= len(dd)-len(cc); n++ {
+			if dd[n] <= maxHead {
+				continue
+			}
+
+			maxHead = dd[n]
+
+			cc[0] = dd[n]
+
+			if len(cc) > 1 {
+				rec(dd[n+1:], cc[1:])
+			}
+		}
+
+		return cc
+	}
+
+	return rec(digits, comb)
 }
--- 2025/day-03/main_test.go
+++ 2025/day-03/main_test.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"slices"
 	"testing"
 )
 
-func TestPart1(t *testing.T) {
-	input := []string{
-		"987654321111111",
-		"811111111111119",
-		"234234234234278",
-		"818181911112111",
-	}
+var input = []string{
+	"987654321111111",
+	"811111111111119",
+	"234234234234278",
+	"818181911112111",
+}
 
+func TestPart1(t *testing.T) {
 	if got, want := part1(input), 357; got != want {
 		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	if got, want := part2(input), 3121910778619; got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestMaxComb(t *testing.T) {
+	digits := []int{1, 5, 4, 3, 5, 6}
+	k := 3
+
+	gotComb := maxComb(digits, k)
+	wantComb := []int{5, 5, 6}
+
+	if !slices.Equal(gotComb, wantComb) {
+		t.Errorf("got %v, want %v", gotComb, wantComb)
 	}
 }
